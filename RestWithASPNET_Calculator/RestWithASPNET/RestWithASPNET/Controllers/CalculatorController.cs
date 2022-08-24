24504c99b3e879411c31444c74128e0b7884a6ea
Adriano Chacheto <adrianochacheto@gmail.com>
2022-08-24 18:44:24 -0300

    Implementação da Calculadora e da mock Person

---
+++ RestWithASPNET_Calculator/RestWithASPNET/RestWithASPNET/Controllers/CalculatorController.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("subtraction/{firstNumber}/{secondNumber}")]
+        public IActionResult Subtraction(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var subtraction = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+
+                return Ok(subtraction.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("multiplication/{firstNumber}/{secondNumber}")]
+        public IActionResult Multiplication(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var multiplication = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+
+                return Ok(multiplication.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("division/{firstNumber}/{secondNumber}")]
+        public IActionResult Division(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var divisor = ConvertToDecimal(secondNumber);
+
+                if (divisor == 0)
+                    return BadRequest("Division by zero");
+
+                var division = ConvertToDecimal(firstNumber) / divisor;
+
+                return Ok(division.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("average/{firstNumber}/{secondNumber}")]
+        public IActionResult Average(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var average = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber)) / 2;
+
+                return Ok(average.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("square-root/{number}")]
+        public IActionResult SquareRoot(string number)
+        {
+            if (IsNumeric(number))
+            {
+                var squareRoot = Math.Sqrt(ConvertToDouble(number));
+
+                return Ok(squareRoot.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        private static decimal ConvertToDecimal(string value)
+        {
+            if (decimal.TryParse(value, out decimal decimalValue))
+                return decimalValue;
+
+            return 0;
+        }
+
+        private static double ConvertToDouble(string value)
+        {
+            if (double.TryParse(value, out double doubleValue))
+                return doubleValue;
+
+            return 0;
+        }
+
+        private static bool IsNumeric(string value)
+        {
+            bool isNumber = double.TryParse(
+                value, 
+                System.Globalization.NumberStyles.Any, 
+                System.Globalization.NumberFormatInfo.InvariantInfo, 
+                out _);
+
+            return isNumber;
+        }
+    }
+}
---
+++ RestWithASPNET_Calculator/RestWithASPNET/RestWithASPNET/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,70 @@
+﻿using RestWithASPNET.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            return;
+        }
+
+        public List<Person> FindAll()
+        {
+            var persons = new List<Person>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                persons.Add(MockPerson(i));
+            }
+            
+            return persons;
+        }
+
+        public Person FindById(long i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Adriano Inacio",
+                LastName = "Chacheto",
+                Address = "São Paulo, SP - Brasil",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name" + i,
+                LastName = "Person Lastname" + i,
+                Address = "Some Address" + i,
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
